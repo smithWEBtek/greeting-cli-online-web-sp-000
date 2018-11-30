0531f4609da49033174ff2988f688b624088a21f
Varisara N <varisara.n@gmail.com>
2018-11-30 18:34:23 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,5 @@
-# code the #greeting method here!
+learn spec/01_greeting_spec.rb# code the #greeting method here!
+def greeting(name)
+  puts "Hi! I'm #{name}, what's your name?"
+  puts "Hello #{name}. It's nice to meet you."
+end
